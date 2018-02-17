1e95197955f6ce10f8bbd38883506426277c3399
nubdub <nabdeep@vt.edu>
2018-02-17 01:24:56 -0500

    This is my first commit of SpaceFighter.

---
+++ app/src/main/java/com/example/nubdub/spacefighter/GameActivity.java
@@ -0,0 +1,8 @@
+package com.example.nubdub.spacefighter;
+
+/**
+ * Created by nubdu on 2/17/2018.
+ */
+
+public class GameActivity {
+}
---
+++ app/src/main/java/com/example/nubdub/spacefighter/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.nubdub.spacefighter;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Image button
+    private ImageButton buttonPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setting the orientation to landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //getting the button
+        buttonPlay = findViewById(R.id.buttonPlay);
+
+        //adding a click listener
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //starting game activity
+        startActivity(new Intent(this, GameActivity.class));
+    }
+}
